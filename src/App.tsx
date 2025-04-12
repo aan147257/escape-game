6c16a433d9ce5db6bff4d62e7e35039ef1a2a68b
lla146116 <lla146116@stud.gibb.ch>
2025-04-12 12:24:01 +0200

    Fügt das Quiz 5 anhand ihren Anforderungen hinzu

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Quiz1 from "./components/pages/Quiz1";
 import Quiz2 from "./components/pages/Quiz2Page";
 import Home from "./components/pages/Home";
 import Quiz4 from "./components/pages/Quiz4Page";
+import Quiz5 from "./components/pages/Quiz5Page";
 import Quiz3 from "./components/pages/Quiz3Page";
 import Quiz7 from "./components/pages/Quiz7Page";
 import Quiz8 from "./components/pages/Quiz8Page";
@@ -20,6 +21,7 @@ function App() {
                 <Route path={"/2"} element={<Quiz2/>}></Route>
                 <Route path={"/3"} element={<Quiz3/>}></Route>
                 <Route path={"/4"} element={<Quiz4/>}></Route>
+                <Route path={"/5"} element={<Quiz5/>}></Route>
                 <Route path={"/7"} element={<Quiz7/>}></Route>
                 <Route path={"/8"} element={<Quiz8/>}></Route>
             </Routes>
